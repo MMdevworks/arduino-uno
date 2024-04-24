c1e42fc44902190acb42d70acaf641937a512f23
Marie Marcos <mmdevq@gmail.com>
2024-04-23 19:47:03 -0700

    digital inputs

---
+++ Digital_Inputs/Toggle_Switch.ino
@@ -0,0 +1,26 @@
+// Make single input button act as a toggle,s delay to prevent multi-toggle on single press
+
+int ledPin = 5;
+int buttonApin = 9;
+int buttonBpin = 8;
+bool toggle = false;
+
+void setup() 
+{
+  pinMode(ledPin, OUTPUT);
+  pinMode(buttonApin, INPUT_PULLUP);  
+}
+
+void loop() 
+{
+  if (digitalRead(buttonApin) == LOW) { 
+    if (toggle == false) {
+    digitalWrite(ledPin, HIGH);
+    toggle = true;
+  } else {
+    digitalWrite(ledPin, LOW);
+    toggle = false;
+  }
+  delay(200);
+  }
+}
