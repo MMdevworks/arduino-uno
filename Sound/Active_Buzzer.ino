37b4abe2f977bc2cc20956c243917b6c9097f65f
Marie Marcos <mmdevq@gmail.com>
2024-04-25 17:50:04 -0700

    actv buzz: refactor 1.1

--- Sound/Active_Buzzer.ino
+++ Sound/Active_Buzzer.ino
@@ -1,65 +1,90 @@
 // Simple Circuit active buzzer tone:
 // Morse Code - "Hello World"
 
-int buzzer = 12; //the pin of the active buzzer
+int buzzer = 12; // pin of the active buzzer
+int delay_short = 200;
+int delay_long = 300;
+int dash = 300;
+int dot = 100;
+
+void morse_dot() {
+  digitalWrite(buzzer, HIGH);   // activate
+  delay(dot);                   // Keep playing sound for
+  digitalWrite(buzzer, LOW);    // deactivate
+  delay(delay_short);           // delay before starting again
+}
+
+void morse_dash(){
+  digitalWrite(buzzer, HIGH);
+  delay(dash);
+  digitalWrite(buzzer, LOW);
+  delay(delay_short);
+}
 
 void setup()
 {
   Serial.begin(9600);
-  pinMode(buzzer, OUTPUT); //initialize the buzzer pin as an output
+  pinMode(buzzer, OUTPUT); 
 }
 
 void loop() {
-int delay_short = 400;
-int delay_long = 600;
-int dash = 300;
-int dot = 100;
-bool played = false;
 
-  if (!played) {
-    Serial.write('H');  // ....
+    // H: ....
     for (int i = 0; i < 4; i++) {
-      digitalWrite(buzzer, HIGH);   // activate
-      delay(dot);                   // Keep playing sound for
-      digitalWrite(buzzer, LOW);    // deactivate
-      delay(delay_short);           // delay before starting again
+      morse_dot();
     }
-
-    Serial.write('E');  // .
-    digitalWrite(buzzer, HIGH);
-    delay(dot);   
-    digitalWrite(buzzer, LOW);   
+    
+    // E: .
+    Serial.write('E');  
+    delay(delay_short);
+    morse_dot();
     delay(delay_long);
 
+    // L: .-..
     for (int i = 0; i < 2; i++) {
-      Serial.write('L');    // .-..
-      digitalWrite(buzzer, HIGH);
-      delay(dot);
-      digitalWrite(buzzer, LOW);
-      delay(delay_short);
-
-      digitalWrite(buzzer, HIGH);
-      delay(dash);
-      digitalWrite(buzzer, LOW);
-      delay(delay_short);
-    
-      digitalWrite(buzzer, HIGH);
-      delay(dot);
-      digitalWrite(buzzer, LOW);
-      delay(delay_short);
-
-      digitalWrite(buzzer, HIGH);
-      delay(dot);
-      digitalWrite(buzzer, LOW);
+      Serial.write('L');    
+      morse_dot();
+      morse_dash();
+      morse_dot();
+      morse_dot();
       delay(delay_short);
     }
+    delay(delay_long);
 
-    Serial.write('O');   // ---
+    // O: ---
     for (int i = 0; i < 3; i++) {
-      digitalWrite(buzzer, HIGH);
-      delay(dash);
-      digitalWrite(buzzer, LOW);
-      delay(delay_short);
+      morse_dash();
     }
-  }
+    delay(delay_long * 2);
+
+    // W: .--
+    morse_dot();
+    morse_dash();
+    morse_dash();
+    delay(delay_long);
+
+    // O: ---
+    for (int i = 0; i < 3; i++) {
+      morse_dash();
+    }
+    delay(delay_long);
+
+    // R: .-.
+    morse_dot();
+    morse_dash();
+    morse_dot();   
+    delay(delay_long);
+
+    // L: .-..
+    morse_dot();
+    morse_dash();
+    morse_dot();
+    morse_dot();
+    delay(delay_long);
+
+    // D: -..
+    morse_dash();
+    morse_dot();
+    morse_dot();
+    delay(delay_long * 4);
 }
