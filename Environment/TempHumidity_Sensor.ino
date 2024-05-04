18291b53ae16f54d7f77a59bdc633a1081185a6a
Marie Marcos <mmdevq@gmail.com>
2024-05-03 19:30:45 -0700

    DHT11 setup

--- Environment/TempHumidity_Sensor.ino
+++ Environment/TempHumidity_Sensor.ino
@@ -1,1 +1,13 @@
 // DHT11 Temperature and Humidity Sensor
+
+#include <dht_nonblocking.h>
+#define DHT_SENSOR_TYPE DHT_TYPE_11
+
+static const int DHT_SENSOR_PIN = 2;
+DHT_nonblocking dht_sensor( DHT_SENSOR_PIN, DHT_SENSOR_TYPE );
+
+
+void setup( )
+{
+  Serial.begin( 9600);
+}
