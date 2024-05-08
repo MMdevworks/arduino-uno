ab9c99de9c38fdaf8a0a51f32a207cac16275e14
Marie Marcos <mmdevq@gmail.com>
2024-05-07 17:40:41 -0700

    analog js partial

--- Movement/Analog_Joystick.ino
+++ Movement/Analog_Joystick.ino
@@ -10,16 +10,16 @@ const int X_pin = A0; // analog pin connected to X output
 const int Y_pin = A1; // analog pin connected to Y output
 
 void setup() {
-  pinMode(RED, OUTPUT);
-  pinMode(GREEN, OUTPUT);
-  pinMode(BLUE, OUTPUT);
-  digitalWrite(RED, HIGH);
-  digitalWrite(GREEN, LOW);
-  digitalWrite(BLUE, LOW);
+    pinMode(RED, OUTPUT);
+    pinMode(GREEN, OUTPUT);
+    pinMode(BLUE, OUTPUT);
+    digitalWrite(RED, HIGH);
+    digitalWrite(GREEN, LOW);
+    digitalWrite(BLUE, LOW);
 
-  pinMode(SW_pin, INPUT);
-  digitalWrite(SW_pin, HIGH);
-  Serial.begin(9600);
+    pinMode(SW_pin, INPUT);
+    digitalWrite(SW_pin, HIGH);
+    Serial.begin(9600);
 }
 
 int redValue;
@@ -28,35 +28,37 @@ int blueValue;
 
 void loop() {
 
-  if (analogRead(X_pin) < 400) { 
-    for(int i = 0; i < 255; i += 1) {
-      redValue -= 1;
-      blueValue += 1;
-      analogWrite(RED, redValue);
-      analogWrite(BLUE, blueValue);
+    if (analogRead(X_pin) < 500){
+        greenValue = 0;
+        blueValue = analogRead(X_pin)/5;
+        redValue = analogRead(Y_pin)/5;
     }
-  }
-  
-  if (analogRead(X_pin) > 500){ 
-    for(int i = 0; i < 255; i += 1) {
-      blueValue -= 1;
-      greenValue += 1;
-      analogWrite(BLUE, blueValue);
-      analogWrite(GREEN, greenValue);
+    else if(analogRead(X_pin) > 500){
+        blueValue = analogRead(X_pin)/5;;
+        greenValue = 0;
+        redValue = analogRead(Y_pin)/5;
     }
-  }
+    else if(analogRead(X_pin) > 400 && analogRead(Y_pin) > 500){
+        blueValue = 0;
+        greenValue = 255;
+        redValue = analogRead(Y_pin) / 5;
+    }
 
-  Serial.print("Switch:  ");
-  Serial.print(digitalRead(SW_pin));
-  Serial.print("\n");
+    analogWrite(RED, redValue);
+    analogWrite(BLUE, blueValue);
+    analogWrite(GREEN, greenValue);
 
-  Serial.print("X-axis: ");
-  Serial.print(analogRead(X_pin));
-  Serial.print("\n");
+    Serial.print("Switch:  ");
+    Serial.print(digitalRead(SW_pin));
+    Serial.print("\n");
 
-  Serial.print("Y-axis: ");
-  Serial.println(analogRead(Y_pin));
-  Serial.print("\n\n");
+    Serial.print("X-axis: ");
+    Serial.print(analogRead(X_pin));
+    Serial.print("\n");
 
-  delay(1000);
+    Serial.print("Y-axis: ");
+    Serial.println(analogRead(Y_pin));
+    Serial.print("\n\n");
+
+    delay(1000);
 }
